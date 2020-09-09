b9b95d05039112a113863d5d0983808e0b2f6b34
po <564987175@qq.com>
2020-09-09 23:15:17 +0800

    活动列表页面

--- miniprogram/pages/activity/activity.js
+++ miniprogram/pages/activity/activity.js
@@ -1,160 +1,38 @@
-// pages/fengguang/fengguang.js
-
 Page({
-
-  data:{
-
  
-
-    "items": [
-
-      {
-
-        "id": "1",
-
-        "imageUrl": "http://xl.tianjiaoedu.org/upload/201607/05/201607051554541730.jpg",
-
-        "content": "南昌校区图书馆"
-
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: [] // 数据列表
+  },
+ 
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) { // options 为 board页传来的参数
+    const _this = this;
+    // 拼接请求url
+    const url = 'https://www.potucs.com/flytosky-1.0-SNAPSHOT/activity/activityList' ;
+    // 请求数据
+    wx.request({
+      url: url,
+      method: 'post',
+      data: {
+       
       },
-
-      {
-
-        "id": "2",
-
-        "imageUrl": "https://www.potucs.com/wechat/activity/mu/zaowanfei.jpg",
-
-        "content": "抚州校区的西湖"
-
+      header: {
+        'content-type': 'application/json' // 默认值
       },
-
-      {
-
-        "id": "3",
-
-        "imageUrl": "https://www.potucs.com/wechat/activity/mu/zaowanfei.jpg",
-
-        "content": "新生军训"
-
-      },
-
-      {
-
-        "id": "4",
-
-        "imageUrl": "http://img2.ph.126.net/NjHvTMhbCihDSocfwxE11g==/2855563638830365544.jpg",
-
-        "content": "樱花广场"
-
-      },
-
-      {
-
-        "id": "5",
-
-        "imageUrl": "http://202.101.244.180/ecitweb2007/news2007/uploadfile/newsimg/165-2.jpg",
-
-        "content": "校园雪景"
-
-      },
-
-      {
-
-        "id": "6",
-
-        "imageUrl": "http://i1.sinaimg.cn/travel/2014/1021/U8842P704DT20141021103442_1.jpg",
-
-        "content": "校园风光"
-
-      },
-
-      {
-
-        "id": "7",
-
-        "imageUrl": "http://202.101.244.180/ecitweb2007/news2007/uploadfile/newsimg/1651-4.jpg",
-
-        "content": "抚州校区夜景"
-
-      },
-
-      {
-
-        "id": "8",
-
-        "imageUrl": "http://upload.ct.youth.cn/2013/0707/1373173834482.jpg",
-
-        "content": "三下乡活动"
-
-      },
-
-      {
-
-        "id": "9",
-
-        "imageUrl": "http://202.101.244.180/ecitweb2007/news2007/uploadfile/newstxt/20160525091829491.jpg",
-
-        "content": "文艺晚会"
-
-      },
-
-      {
-
-        "id": "10",
-
-        "imageUrl": "http://202.101.244.180/ecitweb2007/news2007/uploadfile/newstxt/20151215190012388.jpg",
-
-        "content": "广播操比赛"
-
-      },
-
-      {
-
-        "id": "11",
-
-        "imageUrl": "http://202.101.244.180/ecitweb2007/news2007/uploadfile/newstxt/20151110122039657.jpg",
-
-        "content": "校运会"
-      },
-
-      {
-
-        "id": "12",
-
-        "imageUrl": "http://pic.baike.soso.com/p/20130618/20130618153602-1487877646.jpg",
-
-        "content": "国防生风采"
-
+      success: function(res) {
+        console.log(res.data.data);
+        // 赋值
+        _this.setData({
+          list: res.data.data
+        })
       }
-
-    ]
- 
-
-  },
-
-   
-
-  onReady:function(){
-
-    // 页面渲染完成
-  },
-
-  onShow:function(){
-
-    // 页面显示
-
-  },
-
-  onHide:function(){
-
-    // 页面隐藏
-
-  },
-
-  onUnload:function(){
-
-    // 页面关闭
-
+    })
   }
+
 
 })
