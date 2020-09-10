5c021f4f19a4766bc91334b191a63c3fda857d93
wangzekun <wangzekun@bonc.com.cn>
2020-09-10 15:59:07 +0800

    活动详情页面

--- miniprogram/pages/activitydetail/activitydetail.js
+++ miniprogram/pages/activitydetail/activitydetail.js
@@ -1,106 +1,22 @@
-// pages/inf/inf.js
+let wxparse = require("../../wxParse/wxParse.js");
 Page({
+            data: {
+              dkheight:300,
+              dkcontent: "你好<br/>nihao, <br/><br/><br/><br/><br/><br/><br/>这个是测试<br/><br/>你同意了吗<br/><br/><br/>hehe<b>n你好啊，我加粗了kk</b >",
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    'headLineList': [
-      {
-        'id': '1',
-        'title': '页面功能开发中 ， 敬请期待。。。。',
-        'source': '中国东方航空',
-        'date': '2020-9-10'
-      }
-    ]
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-  // 资讯
-  jumpDetails: function (e) {
-    console.log(e.currentTarget.dataset.title)
-    wx.navigateTo({
-      url: '../news/news?title=' + e.currentTarget.dataset.title,
-      success: function (res) {
-        // success
-      },
-      fail: function () {
-        // fail
-      },
-      complete: function () {
-        // complete
-      }
-    })
-  },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    return {
-      title: '微信小程序联盟',
-      desc: '最具人气的小程序开发联盟!',
-      path: '/page/user?id=123'
-    }
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  }
-})
+              },
+              onLoad: function (options) {
+                // 获得高度
+                let winPage = this;
+                wx.getSystemInfo({
+                  success: function (res) {
+                    let winHeight = res.windowHeight;
+                    console.log(winHeight);
+                    winPage.setData({
+                      dkheight: winHeight - winHeight*0.05 - 80
+                    })
+                  }
+                })
+                wxparse.wxParse('dkcontent', 'html', this.data.dkcontent, this, 5);
+              }
+      })
