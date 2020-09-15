9a287b05d2f29263c569319fc4b9220c2c758446
wangzekun <wangzekun@bonc.com.cn>
2020-09-15 15:11:24 +0800

    航班样式提交

--- miniprogram/pages/city/city.js
+++ miniprogram/pages/city/city.js
@@ -1,25 +1,83 @@
-const app = getApp()
+// pages/flight/index.js
+
+const utils = require('../utils/sutil.js')
+const App = getApp()
 
 Page({
-  data: {
-    winHeight: 0
+  data:{
+    dcity: '',
+    dcityName: '',
+    acity: '',
+    acityName: '',
+    ddate: ''
   },
-  //监听传值，后面自己做处理了
-  cityTap(e) {
-    console.log(e);
-    const cityName = e.detail.cityname;
-    console.log('选择城市为：' + cityName);
-    console.log('此处选择后，跳转页面，将城市名传过去你需要的页面就行了');
+  bindKeyInput: function(e) {
+    const inputId = e.currentTarget.id
+    if (inputId === 'dcity') {  
+      App.setDcity(e.detail.value)
+    } else if (inputId === 'acity') {
+      App.setAcity(e.detail.value)
+    } else if (inputId === 'ddate') {
+      App.setDdate(e.detail.value)
+    }
+    
+    this.getSearchParams()
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    const win = wx.getSystemInfoSync();
-    console.log(win);
-    this.setData({
-      winHeight: win.windowHeight
-    });
-
+  dcityfocusEvent: function(e) {
+    App.setCityTarget(e.currentTarget.id)
+    wx.navigateTo({
+      url: '/components/citypicker/index'
+    })
+  },
+  acityfocusEvent: function(e) {
+    App.setCityTarget(e.currentTarget.id)
+    //console.log(e.currentTarget)
+    wx.navigateTo({
+      url: '/components/citypicker/index'
+    })
+  },
+  getSearchParams: function() {
+    var that = this
+    //调用应用实例的方法获取全局数据
+    App.getSearchParams(function(params){
+      //更新数据
+      // console.log(params)
+      that.setData({
+        dcity:params.dcity,
+        dcityName: params.dcityName,
+        acity:params.acity,
+        acityName: params.acityName,
+        ddate: params.ddate
+      })
+    })
+  },
+  searchFlight: function () {
+    wx.navigateTo({
+      url: 'list'
+    })
+  },
+  onLoad:function(options){    
+    App.setDdate(utils.tomorrow())
+  },
+  onReady:function(){
+    // 页面渲染完成
+    console.log('onReady')
+  },
+  onShow:function(){
+    // 页面显示
+    console.log('onShow')
+    this.getSearchParams()
+  },
+  onHide:function(){
+    // 页面隐藏
+    console.log('onHide')
+  },
+  onUnload:function(){
+    // 页面关闭
+    console.log('onUnload')
+  },
+  bindDateChange: function (e) {
+    App.setDdate(e.detail.value)
+    this.getSearchParams()
   }
 })
---
+++ miniprogram/pages/utils/sutil.js
@@ -0,0 +1,54 @@
+function formatDate(date) {
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var day = date.getDate()
+
+  return [year, month, day].map(formatNumber).join('-')
+}
+
+function formatTime(date) {
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var day = date.getDate()
+
+  var hour = date.getHours()
+  var minute = date.getMinutes()
+  var second = date.getSeconds()
+
+
+  return [year, month, day].map(formatNumber).join('-') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+function formatNumber(n) {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+
+function today() {
+  return addDate(Date.now(), 0)
+}
+
+function tomorrow() {
+  return addDate(Date.now(), 1)
+}
+
+function addDate(d0, x) {
+  var d1 = new Date(d0)
+
+  d1 = d1.valueOf()
+  d1 = d1 + x * 24 * 60 * 60 * 1000
+  d1 = new Date(d1)
+
+  const year = d1.getFullYear()
+  const month = d1.getMonth() + 1
+  const day = d1.getDate()
+
+  return [year, month, day].map(formatNumber).join('-')
+}
+
+module.exports = {
+  formatTime: formatTime,
+  formatDate: formatDate,
+  today,
+  tomorrow
+}
