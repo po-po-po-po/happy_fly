6a17b867d5a420da30e3401fb4ceeabbb0bcb4e2
wangzekun <wangzekun@bonc.com.cn>
2020-09-16 11:20:32 +0800

    航空公司js提交

--- miniprogram/pages/utils/airlines0.js
+++ miniprogram/pages/utils/airlines0.js
@@ -4,12 +4,47 @@ var hotCities=[];
 //初始化所有国内机场城市
 var citys=[];
 citys[0] = new Array('CA','中国国际航空','国航','国航系');
-citys[1] = new Array('MU','东方航空','东航','东航系');
-citys[2] = new Array('CZ','南方航空','南航','南航系');
+citys[1] = new Array('MU','中国东方航空|上海航空','东航','东航系');
+citys[2] = new Array('CZ','中国南方航空|重庆航空','南航','南航系');
 citys[3] = new Array('FM','上海航空','上航','东航系');
 citys[4] = new Array('MF','厦门航空','厦航','南航系');
 citys[5] = new Array('ZH','深圳航空','深航','国航系');
 citys[6] = new Array('SC','山东航空','山航','国航系');
+citys[7] = new Array('KN','中国联合航空','中国联航','东航系');
+citys[8] = new Array('HU','海南航空|大新华航空','东航','海航系');
+citys[9] = new Array('JD','首都航空','首都航空','海航系');
+citys[10] = new Array('GJ','浙江长龙航空','长龙航空','');
+citys[11] = new Array('NS','河北航空','河北航','南方系');
+citys[12] = new Array('HO','吉祥航空','吉祥','');
+citys[13] = new Array('9C','春秋航空','春秋','');
+citys[14] = new Array('3U','四川航空','川航','');
+citys[15] = new Array('EU','成都航空','成都航','');
+citys[16] = new Array('TV','西藏航空','藏航','');
+citys[17] = new Array('GS','天津航空','天航','');
+citys[18] = new Array('DZ','东海航空','东海航','');
+citys[19] = new Array('Y8','金鹏航空','金鹏航空','');
+citys[20] = new Array('KY','昆明航空','昆明航空','');
+citys[21] = new Array('8L','祥鹏航空','祥鹏','海航系');
+citys[22] = new Array('DR','瑞丽航空','瑞丽','');
+citys[23] = new Array('QW','青岛航空','青岛航','');
+citys[24] = new Array('BK','奥凯航空','奥凯','');
+citys[25] = new Array('G5','华夏航空','华夏','');
+citys[26] = new Array('PN','西部航空','西部航空','海航系');
+citys[27] = new Array('A6','红土航空','红土','');
+citys[28] = new Array('AQ','九元航空','九元','');
+citys[29] = new Array('FU','福州航空','福州航空','海航系');
+citys[30] = new Array('GT','桂林航空','桂林航空','');
+citys[31] = new Array('CN','大新华航空','大新华航空','海航系');
+citys[32] = new Array('LT','龙江航空','龙江航空','');
+citys[33] = new Array('JR','幸福航空','幸福航空','');
+citys[34] = new Array('UQ','乌鲁木齐航空','乌鲁木齐航空','海航系');
+citys[35] = new Array('RY','江西航空','江西航空','');
+citys[36] = new Array('GY','多彩贵州航空','多彩贵州航空','');
+citys[37] = new Array('GX','广西北部湾航空','北部湾','海航系');
+citys[38] = new Array('9H','长安航空','长安航空','海航系');
+
+
+
 module.exports = {
 	hotCities, 
 	cities: citys
