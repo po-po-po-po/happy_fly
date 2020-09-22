df3084f764f25cd09588faf870cd43eb00d8e1e7
wangzekun <wangzekun@bonc.com.cn>
2020-09-22 12:32:11 +0800

    机场指南

--- miniprogram/pages/sut/index.js
+++ miniprogram/pages/sut/index.js
@@ -8,7 +8,7 @@ Page({
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     menuitems: [
-      { text: '功能反馈', url: '../message/message?userInfo=userInfo', icon: 'http://www.potucs.com:9998/wechat/logo/fankui.png', tips: '' }
+      { text: '问题|建议', url: '../message/message?userInfo=userInfo', icon: 'http://www.potucs.com:9998/wechat/logo/fankui.png', tips: '' }
       /** ,
       { text: '使用说明', url: '../info/info', icon: 'http://www.potucs.com:9998/wechat/logo/use.png', tips: '' }*/
     ]
