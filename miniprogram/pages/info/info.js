59209d459fe5a3b8e9847d94d19a872dad86e8ac
wangzekun <wangzekun@bonc.com.cn>
2020-09-27 11:34:10 +0800

    我的行程样式修改

--- miniprogram/pages/info/info.js
+++ miniprogram/pages/info/info.js
@@ -1,85 +1,149 @@
-// miniprogram/pages/info/info.js
+// pages/inf/inf.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    'headLineList': [
+      {
+        'title': 'MU7850 广州白云-杭州萧山',
+        'source': '中国东方航空',
+        'date': '2017-9-10'
+      }, {
+        'title': 'MU7850 广州白云-杭州萧山',
+        'source': '中国东方航空',
+        'date': '2017-9-10'
+      }, {
+        'title': 'MU7850 广州白云-杭州萧山',
+        'source': '中国东方航空',
+        'date': '2017-9-10'
+      },
+      {
+        'title': 'MU7850 广州白云-杭州萧山',
+        'source': '中国东方航空',
+        'date': '2017-9-10'
+      }, {
+        'title': 'MU7850 广州白云-杭州萧山',
+        'source': '中国东方航空',
+        'date': '2017-9-10'
+      },
+      {
+        'title': 'MU7850 广州白云-杭州萧山',
+        'source': '中国东方航空',
+        'date': '2017-9-10'
+      }, {
+        'title': 'MU7850 广州白云-杭州萧山',
+        'source': '中国东方航空',
+        'date': '2017-9-10'
+      },
+      {
+        'title': 'MU7850 广州白云-杭州萧山',
+        'source': '中国东方航空',
+        'date': '2017-9-10'
+      }, {
+        'title': 'MU7850 广州白云-杭州萧山',
+        'source': '中国东方航空',
+        'date': '2017-9-10'
+      },
+      {
+        'title': 'MU7850 广州白云-杭州萧山',
+        'source': '中国东方航空',
+        'date': '2017-9-10'
+      }, {
+        'title': 'MU7850 广州白云-杭州萧山',
+        'source': '中国东方航空',
+        'date': '2017-9-10'
+      }
+    ],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) { // options 为 board页传来的参数
-    const _this = this;
-    // 拼接请求url
-    const url = 'https://www.potucs.com/flytosky-1.0-SNAPSHOT/wxMessage/info' ;
-    // 请求数据
-    wx.request({
-      url: url,
-      method: 'post',
-      data: {
+  onLoad: function (options) {
+  
+  },
+  // 资讯
+  jumpDetails: function (e) {
+    console.log(e.currentTarget.dataset.title)
+    wx.navigateTo({
+      url: '../news/news?title=' + e.currentTarget.dataset.title,
+      success: function (res) {
+        // success
       },
-      header: {
-        'content-type': 'application/json' // 默认值
+      fail: function () {
+        // fail
       },
-      success: function(res) {
-        console.log(res.data);
-        // 赋值
-        _this.setData({
-          info: res.data.data
-        })
+      complete: function () {
+        // complete
       }
     })
   },
-
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+  
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+  
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+  
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
+    return {
+      title: '微信小程序联盟',
+      desc: '最具人气的小程序开发联盟!',
+      path: '/page/user?id=123'
+    }
+  },
 
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
   }
 })
