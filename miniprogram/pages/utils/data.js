c6adbc9b8eddeb0c3efdc1ed9949fdb16153a4a4
wangzekun <wangzekun@bonc.com.cn>
2020-09-29 15:06:43 +0800

    我的足迹

--- miniprogram/pages/utils/data.js
+++ miniprogram/pages/utils/data.js
@@ -1,55 +1,73 @@
 module.exports = [
   {
     "id": 1,
-    "name": "",
+    "name": "北京大兴-乌鲁木齐地窝堡",
     "longitude": "115.826413",
     "latitude": "28.627321"
   },
   {
     "id": 2,
-    "name": "",
+    "name": "北京首都-青岛流亭",
     "longitude": "118.669223",
     "latitude": "32.049534"
   },
   {
     "id": 3,
-    "name": "",
+    "name": "北京首都-三亚凤凰",
     "longitude": "113.847106",
     "latitude": "23.001823"
   },
   {
     "id": 4,
-    "name": "",
+    "name": "北京首都-重庆江北",
     "longitude": "87.616127",
     "latitude": "43.815061"
   },
   {
     "id": 5,
-    "name": "",
+    "name": "北京首都-哈尔滨太平",
     "longitude": "123.465109",
     "latitude": "41.746114"
   },
   {
     "id": 6,
-    "name": "",
+    "name": "深圳宝安-桂林两江",
     "longitude": "117.197563",
     "latitude": "36.693114"
   },
   {
     "id": 7,
-    "name": "",
+    "name": "北京大兴-重庆江北",
     "longitude": "120.259312",
     "latitude": "29.757777"
   },
   {
     "id": 8,
-    "name": "",
+    "name": "上海虹桥-成都双流",
     "longitude": "114.223228",
     "latitude": "22.703872"
   },
   {
     "id": 9,
-    "name": "",
+    "name": "北京首都-成都双流",
+    "longitude": "113.329711",
+    "latitude": "23.141123"
+  },
+  {
+    "id": 10,
+    "name": "广州白云-成都双流",
+    "longitude": "113.329711",
+    "latitude": "23.141123"
+  },
+  {
+    "id": 11,
+    "name": "西安咸阳-成都双流",
+    "longitude": "113.329711",
+    "latitude": "23.141123"
+  },
+  {
+    "id": 12,
+    "name": "上海虹桥-成都双流",
     "longitude": "113.329711",
     "latitude": "23.141123"
   }
