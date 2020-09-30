3377c2efa562ac623741af2724d938ffe71b9b8c
wangzekun <wangzekun@bonc.com.cn>
2020-09-30 13:16:53 +0800

    行程统计

--- miniprogram/pages/schedulingdetail/scheduling.js
+++ miniprogram/pages/schedulingdetail/scheduling.js
@@ -7,25 +7,15 @@ Page({
    */
   data: {
     listData:[
-      
-            {"code":"01","text":"text1","type":"type1"},
-      
-            {"code":"02","text":"text2","type":"type2"},
-      
-            {"code":"03","text":"text3","type":"type3"},
-      
-            {"code":"04","text":"text4","type":"type4"},
-      
-            {"code":"05","text":"text5","type":"type5"},
-      
-            {"code":"06","text":"text6","type":"type6"},
-      
-            {"code":"07","text":"text7","type":"type7"}
-      
+            {"code":"01","text":"南方航空","type":"45"},
+            {"code":"02","text":"东方航空","type":"32"},
+            {"code":"03","text":"深圳航空","type":"31"},
+            {"code":"04","text":"中国国航","type":"23"},
+            {"code":"05","text":"海南航空","type":"23"},
+            {"code":"06","text":"四川航空","type":"12"},
+            {"code":"07","text":"厦门航空","type":"5"}
           ]
-      
         }
-      
   ,
 
   /**
