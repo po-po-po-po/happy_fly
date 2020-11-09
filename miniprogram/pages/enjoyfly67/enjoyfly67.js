a733c9ed236ef60ac06d51c55eb075ab899664ad
wangzekun <wangzekun@bonc.com.cn>
2020-11-09 11:39:55 +0800

    周末随心飞

--- miniprogram/pages/enjoyfly67/enjoyfly67.js
+++ miniprogram/pages/enjoyfly67/enjoyfly67.js
@@ -1,40 +1,46 @@
-let wxparse = require("../../wxParse/wxParse.js");
-Page({
-            data: {
-              dkheight:300,
-              dkcontent: "",
-              },
-              onLoad: function (options) {
-                // 获得高度
-                let winPage = this;
-                wx.getSystemInfo({
-                  success: function (res) {
-                    let winHeight = res.windowHeight;
-                    console.log(winHeight);
-                    winPage.setData({
-                      dkheight: winHeight
-                    })
-                  }
-                }),
-                wx.request({
-                  url: 'https://www.potucs.com/flytosky-2.0-SNAPSHOT/activity/activityDetail',
-                  method: 'post',
-                  data: {
-                    id:options.id,
-                    airportCode:options.airportCode
-                  },
-                  header: {
-                    'content-type': 'application/json' // 默认值
-                  },
-                  success: function(res) {
-                    console.log("www"+ res.data.data)
-                    // 赋值
-                    winPage.setData({
-                      dkcontent:  wxparse.wxParse('dkcontent', 'html', res.data.data.content, winPage, 5)
-                    })
-                  }
-                })
-              
-              },
-            
-      })
+page({
+  data: {
+  // 搜索框状态
+  inputshowed: false,
+  // 搜索框值
+  inputval: "",
+  //搜索渲染推荐数据
+  list:[
+   {name:"帝霸"},
+   { name: "遮天" },
+   { name: "道界天下" },
+   { name: "菜鸟" },
+   { name: "先飞" }
+  ]
+  },
+  // 显示搜索框
+  showinput: function () {
+  this.setdata({
+   inputshowed: true
+  });
+  },
+  // 隐藏搜索框样式
+  hideinput: function () {
+  this.setdata({
+   inputval: "",
+   inputshowed: false
+  });
+  },
+  // 清除搜索框值
+  clearinput: function () {
+  this.setdata({
+   inputval: ""
+  });
+  },
+  // 获取搜索框值
+  inputtyping: function (e) {
+  this.setdata({
+   inputval: e.detail.value
+  });
+  console.log(this.data.inputval);
+  },
+  // 获取选中推荐列表中的值
+  btn_name:function(res){
+  console.log(res.currenttarget.dataset.index, res.currenttarget.dataset.name);
+  }
+ });
