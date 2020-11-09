e380083ef0912984149a1fbc5ca752bcf81bc376
wangzekun <wangzekun@bonc.com.cn>
2020-11-09 20:11:44 +0800

    春秋飞

---
+++ miniprogram/pages/flyzw/fly67.js
@@ -0,0 +1,105 @@
+// pages/flight/index.js
+
+const utils = require('../utils/sutil.js')
+const App = getApp()
+
+Page({
+  data:{
+    dcity: '',
+    dcityName: '',
+    acity: '',
+    acityName: '',
+    airlineCode:'',
+    airlineName:'',
+    weekCode:'',
+    weekName:'',
+    ddate: '',
+    ddate1: '',
+    day:''
+  },
+  bindKeyInput: function(e) {
+    const inputId = e.currentTarget.id
+    if (inputId === 'dcity') {  
+      App.setDcity(e.detail.value)
+    } else if (inputId === 'acity') {
+      App.setAcity(e.detail.value)
+    } else if (inputId === 'ddate') {
+      App.setDdate(e.detail.value)
+    }else if (inputId === 'airlineCode') {
+      App.setDdate(e.detail.value)
+    }else if (inputId === 'ddate1') {
+      App.setDdate1(e.detail.value)
+    }
+    else if (inputId === 'weekCode') {
+      App.setWeekCode(e.detail.value)
+    }
+    
+    this.getSearchParams()
+  },
+  dcityfocusEvent: function(e) {
+    App.setCityTarget(e.currentTarget.id)
+    wx.navigateTo({
+      url: '/components/citypicker/index'
+    })
+  },
+  acityfocusEvent: function(e) {
+    App.setCityTarget(e.currentTarget.id)
+    //console.log(e.currentTarget)
+    wx.navigateTo({
+      url: '/components/citypicker/index'
+    })
+  },
+  getSearchParams: function() {
+    var that = this
+    //调用应用实例的方法获取全局数据
+    App.getSearchParams(function(params){
+      //更新数据
+       console.log(params)
+      that.setData({
+        dcity:params.dcity,
+        dcityName: params.dcityName,
+        acity:params.acity,
+        acityName: params.acityName,
+        airlineCode:params.airlineCode,
+        airlineName: params.airlineName,
+        weekCode:params.weekCode,
+        weekName: params.weekName,
+        ddate: params.ddate,
+        ddate1: params.ddate1
+      })
+    })
+  },
+  searchFlight: function () {
+    wx.navigateTo({
+      url: 'list?day='+App.globalData.searchParams.day
+    })
+  },
+  onLoad:function(options){
+    App.setDay(options.day)
+  },
+  onReady:function(){
+    // 页面渲染完成
+    console.log('onReady')
+  },
+  onShow:function(){
+    // 页面显示
+    console.log('onShow')
+    this.getSearchParams()
+  },
+  onHide:function(){
+    // 页面隐藏
+    console.log('onHide')
+  },
+  onUnload:function(){
+    // 页面关闭
+    console.log('onUnload')
+  },
+  bindDateChange: function (e) {
+    App.setDdate(e.detail.value)
+    this.getSearchParams()
+  },
+  bindDateChange1: function (e) {
+    App.setDdate1(e.detail.value)
+    this.getSearchParams()
+  }
+})
---
+++ miniprogram/pages/flyzw/list.js
@@ -0,0 +1,142 @@
+//获取公共ui操作类实例
+const _page = require('../utils/abstract-page.js');
+let modCalendar = require('../utils/calendar.js');
+const models = require('../utils/demo-model.js')
+const util = require('../utils/uti1l.js')
+let selectedDate = new Date().toString();
+const App = getApp()
+
+//获取应用实例
+const app = getApp()
+
+Page(_page.initPage({
+  data: {
+    listData: [],
+    calendarSelectedDate: '东航周六航班',
+    calendarSelectedDateStr: '东航周六航班',
+    flightNameStart:'上海虹桥',
+    flightNameEnd:'',
+    day:''
+  },
+  // methods: uiUtil.getPageMethods(),
+  methods: {
+  },
+
+  preDay: function () {
+    this.setData({
+      calendarSelectedDate: '周六',
+      calendarSelectedDateStr: '东航周六航班'
+    })
+  
+  },
+
+  nextDay: function () {
+   // let date = util.dateUtil.nextDay(this.data.calendarSelectedDate);
+    this.setData({
+      calendarSelectedDate: '周日',
+      calendarSelectedDateStr: '东航周日航班'
+    })
+  },
+
+  calendarHook: function (date) {
+    this.index = 0;
+    this.data.listData = [];
+    this._initData({
+      date: date.getTime()
+    });
+  },
+
+  onShow: function () {
+    global.sss = this;
+    let scope = this;
+  },
+
+  _setDateInfo: function (date) {
+    let selectedDate = new Date(date * 1);
+    this.setData({
+      calendarSelectedDate: selectedDate.toString(),
+      calendarSelectedDateStr: util.dateUtil.format(selectedDate, 'Y年M月D日')
+    });
+  },
+
+  _appendList: function (data) {
+
+    for(let i = 0, len = data.length; i < len; i++) {
+      data[i].dateStr = util.dateUtil.format(new Date(data[i].datetime * 1000), 'H:F' )
+    }
+
+    this.setData({
+      listData: this.data.listData.concat(data)
+    });
+  },
+  _initData: function (data) {
+    let scope = this;
+    let listModel = models.listModel;
+
+    listModel.setParam({
+      startcityid: this._sid,
+      arrivalcityid: this._aid,
+      startdatetime: data.date / 1000,
+      page: this.index + 1
+    });
+
+    this.showLoading();
+    listModel.execute(function(data) {
+      scope.hideLoading();
+
+      if(!data.schedules) return;
+      scope._appendList(data.schedules);
+
+    });
+  },
+  onLoad: function (options) {
+    const _this = this;
+    console.log("1111")
+        //调用应用实例的方法获取全局数据
+        app.getSearchParams(function(params){    
+          //更新数据
+          _this.setData({
+            dcity:params.dcity,
+            flightNameStart: params.dcityName,
+            acity:params.acity,
+            flightNameEnd: params.acityName,
+          })
+        })
+        var day=App.globalData.searchParams.day;
+    // 拼接请求url
+    const url = 'https://www.potucs.com/flytosky-2.0-SNAPSHOT/flight/findMUFlights'+day;
+    // 请求数据
+    wx.request({
+      url: url,
+      method: 'post',
+      data: {
+        "pageSize": 500,
+        airportNameStartCode:this.data.dcity,
+        airportNameEndCode:this.data.acity
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function(res) {
+        // 赋值
+        _this.setData({
+          list: res.data.data.data,
+          loading: false // 关闭等待框
+        })
+      }
+    })
+
+  },
+
+  onReady: function () {
+
+  },
+  onShow: function () {
+
+    global.sss = this;
+    let scope = this;
+  }
+  
+}, {
+  modCalendar: modCalendar
+}))
