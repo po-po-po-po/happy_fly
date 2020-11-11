4b9aa0bc2242b2c66d3bbe94b3d6102249c57079
wangzekun <wangzekun@bonc.com.cn>
2020-11-11 12:48:02 +0800

    航空公司首页

--- miniprogram/pages/airlines/index.js
+++ miniprogram/pages/airlines/index.js
@@ -1,15 +1,11 @@
-//index.js
-const app = getApp()
-
 Page({
+ 
+  /**
+   * 页面的初始数据
+   */
   data: {
-    avatarUrl: './user-unlogin.png',
-    userInfo: {},
-    logged: false,
-    takeSession: false,
-    requestResult: ''
+    list: [] // 数据列表
   },
-
   onLoad: function (options) { // options 为 board页传来的参数
     const _this = this;
     // 拼接请求url
@@ -25,40 +21,16 @@ Page({
         'content-type': 'application/json' // 默认值
       },
       success: function(res) {
-        console.log(res.data);
+        console.log(res.data.data);
         // 赋值
         _this.setData({
-          title: '热门机场TOP50',
+          title: '航空公司',
           airlines: res.data.data,
           loading: false // 关闭等待框
         })
       }
     })
-  },
-
-
-
-  onGetOpenid: function() {
-    // 调用云函数
-    wx.cloud.callFunction({
-      name: 'login',
-      data: {},
-      success: res => {
-        console.log('[云函数] [login] user openid: ', res.result.openid)
-        app.globalData.openid = res.result.openid
-        wx.navigateTo({
-          url: '../userConsole/userConsole',
-        })
-      },
-      fail: err => {
-        console.error('[云函数] [login] 调用失败', err)
-        wx.navigateTo({
-          url: '../deployFunctions/deployFunctions',
-        })
-      }
-    })
   }
-
 
 
 })
